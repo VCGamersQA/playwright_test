f3709b7bf22bfc7d0cc8e438dcae2cdd33632771
ifs20039-itdel <ketrinasiagian14@gmail.com>
2023-06-22 13:23:09 +0700

    second

--- test1/cart.spec.js
+++ test1/cart.spec.js
@@ -1,13 +1,38 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('login test', async ({ page }) => {
-  await page.goto('https://auth.vcgamers.io/login');
+async function valid_login(page) {
+    await page.goto("https://auth.vcgamers.io/login/marketplace?/");
 
-  await page.locator('[placeholder="Email"]').fill('tokoa@mailinator.com');
-  await page.locator('[placeholder="Password"]').fill('ciwkiciw');
-  await page.locator('[class="btn-submit py-3 w-full mt-7 text-center font-semibold text-sm"]').click();
-  await page.locator('#home-instant-slider-3 > div:nth-child(2) > a').click();
-  await page.locator('#__layout > div > div.content-grid.pb-0.ptcustom > div > div.grid.grid-8-4.centered > div.marketplace-sidebar.side-sticky > div.sidebar-box > div > div.action-container.d-flex > div.add-cart.w-100 > button').click(); 
+    await page.locator("//input[@placeholder='Email']").fill("aknrandmm160@gmail.com");
+    await page.locator("//input[@placeholder='Password']").fill("ariframadhan18");
+
+    await page.getByRole('button', { name: 'Masuk' }).click();
+}
+
+test("add to chart", async ({ page }) => {
+    await valid_login(page);
+    await page.locator("(//div[@class='content-text-container'])[21]").click();
+    await page.locator("//div[@class='d-flex product-information-container']").isVisible();
+
+    await page.locator("//button[normalize-space()='Tambah Keranjang']").isVisible();
+    await page.locator("//button[normalize-space()='Tambah Keranjang']").click();
+
+    await page.locator("//h5[normalize-space()='Berhasil di Tambahkan ke Keranjang']").isVisible();
+    await page.locator("//a[normalize-space()='Lihat Keranjang']").click();
+
+    //async function scrollDown() {
+    //  let isVisible = false;
+    //while (!isVisible) {
+    //  await page.evaluate(() => {
+    //    window.scrollBy(0, 200);
+    //});
+    //isVisible = await buttonLocator.isVisible();
+    //await page.waitForTimeout(500);
+    //}
+    //}
+
+    //await scrollDown();
+
+    //await buttonLocator.click();
 
 });
--- test1/login.spec.js
+++ test1/login.spec.js
@@ -1,12 +1,39 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('login test', async ({ page }) => {
-  await page.goto('https://auth.vcgamers.io/login');
+test("valid_login", async ({ page }) => {
+  await page.goto("https://auth.vcgamers.io/login/marketplace?/")
 
-  await page.locator('[placeholder="Email"]').fill('tokoa@mailinator.com');
-  await page.locator('[placeholder="Password"]').fill('ciwkiciw');
-  await page.locator('[class="btn-submit py-3 w-full mt-7 text-center font-semibold text-sm"]').click();
+  await page.locator("//input[@placeholder='Email']").fill("aknrandmm160@gmail.com")
+  await page.locator("//input[@placeholder='Password']").fill("ariframadhan18")
 
+  await page.getByRole('button', { name: 'Masuk' }).click()
+  await expect(page).toHaveURL('https://app.vcgamers.io/')
+  await page.close();
 });
 
+
+test("invalid login wrong email or password", async ({ page }) => {
+  await page.goto("https://auth.vcgamers.io/login/marketplace?/")
+
+  await page.locator("//input[@placeholder='Email']").fill("aknrandmm16@gmail.com")
+  await page.locator("//input[@placeholder='Password']").fill("ariframadhan18")
+
+  await page.getByRole('button', { name: 'Masuk' }).click()
+
+  await expect(page.getByText('Email atau password anda salah')).toBeVisible()
+
+  await page.close();
+});
+
+test("invalid login random character", async ({ page }) => {
+  await page.goto("https://auth.vcgamers.io/login/marketplace?/")
+
+  await page.locator("//input[@placeholder='Email']").fill("aknrandmm162")
+  await page.locator("//input[@placeholder='Password']").fill("ariframadhan18")
+
+  await page.getByRole('button', { name: 'Masuk' }).click()
+
+  await expect(page.getByText('Pastikan email yang kamu tulis sudah benar')).toBeVisible()
+
+  await page.close();
+});
